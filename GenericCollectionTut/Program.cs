4cb02ed63296580c897b85add7ebf14019f2229f
mathews225 <petemathews14@gmail.com>
2021-02-19 10:15:50 -0500

    Add project files.

---
+++ GenericCollectionTut/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCollectionTut {
+	class Program {
+		static void Main(string[] args) {
+
+			var ints = new List<int>();
+			ints.Add(7);
+			ints.Add(10);
+			ints.Add(3);
+			ints.Add(6);
+			ints.Add(96);
+			ints.Add(22);
+
+			foreach (var item in ints) {
+				Console.WriteLine($"Favorite number is {item}.");
+			}
+
+			var strs = new List<string> {
+				"orange",
+				"blue",
+				"gray",
+				"red",
+				"black",
+				"green"
+			};
+			Console.WriteLine($"\nFavorite color count is {strs.Count}.\n");
+			strs.Sort();
+			foreach (var item in strs) {
+				Console.WriteLine($"Favorite color is {item}.");
+			}
+
+
+		}
+	}
+}
---
+++ GenericCollectionTut/Weather.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericCollectionTut {
+	class Weather {
+		public DateTime Time { get; set; } = DateTime.Now;
+		public int Temp { get; set; }
+		public double Precipitation { get; set; }
+	}
+}
